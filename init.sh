45dc71e8e193497e62485a23699eb9556987247d
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-10-17 17:53:33 +0100

    Add .config/autostart

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 rm ~/.inputrc
 rm ~/.profile
 rm ~/.gitconfig
+rm ~/.config/autostart
 
 ln -s ~/.dotfiles/.inputrc ~/.inputrc
 ln -s ~/.dotfiles/.profile ~/.profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/autostart ~/.config/autostart
