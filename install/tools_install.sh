92e64b2e9c9a9c212dd774c9749a20206f7a18e0
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-06-13 12:33:02 +0200

    Add virtualbox to installation and deactivate idea

--- install/tools_install.sh
+++ install/tools_install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ -z "$(which idea)" ] ; then
-    echo "++++++ Installing Idea"
-    add-apt-repository ppa:webupd8team/java
-    apt-get update
-    apt-get install oracle-java7-installer
-    echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select \
-        true | sudo /usr/bin/debconf-set-selections
-    update-java-alternatives -s java-7-oracle
-    wget -O ~/.dotfiles/bin/intellij.tar.gz \
-        http://download.jetbrains.com/idea/ideaIC-14.0.2.tar.gz
-    tar xfz ~/.dotfiles/bin/intellij.tar.gz
-    rm -v ~/.dotfiles/bin/intellij.tar.gz
-    mv ~/.dotfiles/bin/idea-IC-139.659.2 ~/.dotfiles/bin/idea-dir
-else
-    echo "====== Idea is already installed"
-fi
+# if [ -z "$(which idea)" ] ; then
+#     echo "++++++ Installing Idea"
+#     add-apt-repository ppa:webupd8team/java
+#     apt-get update
+#     apt-get install oracle-java7-installer
+#     echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select \
+#         true | sudo /usr/bin/debconf-set-selections
+#     update-java-alternatives -s java-7-oracle
+#     wget -O ~/.dotfiles/bin/intellij.tar.gz \
+#         http://download.jetbrains.com/idea/ideaIC-14.0.2.tar.gz
+#     tar xfz ~/.dotfiles/bin/intellij.tar.gz
+#     rm -v ~/.dotfiles/bin/intellij.tar.gz
+#     mv ~/.dotfiles/bin/idea-IC-139.659.2 ~/.dotfiles/bin/idea-dir
+# else
+#     echo "====== Idea is already installed"
+# fi
 
 if [ -z "$(which terminator)" ] ; then
     echo "++++++ Installing Terminator"
@@ -29,4 +29,11 @@ if [ -z "$(which xmonad)" ] ; then
     sudo apt-get install xmonad gnome-panel suckless-tools
 else
     echo "====== Xmonad is already installed"
+fi
+
+if [ -z "$(which virtualbox)" ] ; then
+    echo "++++++ Installing Virtualbox"
+    sudo apt-get install virtualbox
+else
+    echo "====== Virtualbox is already installed"
 fi
