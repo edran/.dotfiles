09628880415bbf8593503a97fe956bfb7ac8c9f0
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-02-12 21:36:42 +0000

    Install functions + Trusty travis setup

--- install.sh
+++ install.sh
@@ -8,30 +8,31 @@ set -u
 BACKUP="$HOME/.home_backup"
 DOTF="$(pwd)"
 
-if [[ ! "$DOTF" == *.dotfiles ]]; then
-    print_rd "Please run this script from the .dotfiles directory\n"
-    print_rd "cd .dotfiles; ./install.sh\n"
-    exit 1
-fi
+initial_check () {
+    if [[ ! "$DOTF" == *.dotfiles ]]; then
+        print_rd "Please run this script from the .dotfiles directory\n"
+        print_rd "cd .dotfiles; ./install.sh\n"
+        exit 1
+    fi
 
-if [[ $UID != 0 ]]; then
-    print_rd "Please run this script with sudo:\n"
-    print_rd "sudo $0 $*\n"
-    exit 1
-fi
+    if [[ $UID != 0 ]]; then
+        print_rd "Please run this script with sudo:\n"
+        print_rd "sudo $0 $*\n"
+        exit 1
+    fi
 
-print_yl "This script is going to remove many files in your home.\n"
-print_yl "Are you sure you want to run it? [Y,n]"
-read -p " " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+    print_yl "This script is going to remove many files in your home.\n"
+    print_yl "Are you sure you want to run it? [Y,n]"
+    read -p " " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo # for newline
+        exit 1
+    fi
+
     echo # for newline
-    exit 1
-fi
 
-echo # for newline
-
-print_bl "~~~~~~~~~~~~ Backupping home config files... ~~~~~~~~~~~~\n"
+}
 
 backup_file () {
     if [[ ! -f "$1" && ! -d "$1" ]]; then
@@ -41,41 +42,58 @@ backup_file () {
     fi
 }
 
-# Creating / Cleaning up backup folder
-if [ ! -d "$BACKUP" ]; then
-    mkdir -v $BACKUP
-else
-    rm -rf $BACKUP
-    mkdir -v $BACKUP
-fi
+backup_everything () {
+    # Creating / Cleaning up backup folder
+    if [ ! -d "$BACKUP" ]; then
+        mkdir -v $BACKUP
+    else
+        rm -rf $BACKUP
+        mkdir -v $BACKUP
+    fi
 
-backup_file $HOME/.inputrc
-backup_file $HOME/.profile
-backup_file $HOME/.gitconfig
-backup_file $HOME/.bashrc
-backup_file $HOME/.bash_aliases
-backup_file $HOME/bin
-backup_file $HOME/.config
-backup_file $HOME/.fonts
-backup_file $HOME/.tmux.conf
-backup_file $HOME/.Xresources
-backup_file $HOME/.urxvt
-backup_file $HOME/.emacs.d
+    backup_file $HOME/.inputrc
+    backup_file $HOME/.profile
+    backup_file $HOME/.gitconfig
+    backup_file $HOME/.bashrc
+    backup_file $HOME/.bash_aliases
+    backup_file $HOME/bin
+    backup_file $HOME/.config
+    backup_file $HOME/.fonts
+    backup_file $HOME/.tmux.conf
+    backup_file $HOME/.Xresources
+    backup_file $HOME/.urxvt
+    backup_file $HOME/.emacs.d
+
+}
+
+
+link_everything () {
+    ln -sv $DOTF/inputrc $HOME/.inputrc
+    ln -sv $DOTF/profile $HOME/.profile
+    ln -sv $DOTF/gitconfig $HOME/.gitconfig
+    ln -sv $DOTF/bashrc $HOME/.bashrc
+    ln -sv $DOTF/bin $HOME/bin
+    ln -sv $DOTF/config $HOME/.config
+    ln -sv $DOTF/fonts $HOME/.fonts
+    ln -sv $DOTF/venvs $HOME/venvs
+    ln -sv $DOTF/tmux.conf $HOME/.tmux.conf
+    ln -sv $DOTF/Xresources $HOME/.Xresources
+    ln -sv $DOTF/urxvt $HOME/.urxvt
+    ln -sv $DOTF/spacemacs $HOME/.spacemacs.d
+}
+
+
+# -------------------------------
+
+initial_check
+
+print_bl "~~~~~~~~~~~~ Backupping home config files... ~~~~~~~~~~~~\n"
+
+backup_everything
 
 print_bl "~~~~~~~~~~~~~~~~~~~ Linking dotfiles ~~~~~~~~~~~~~~~~~~~~\n"
 
-ln -sv $DOTF/inputrc $HOME/.inputrc
-ln -sv $DOTF/profile $HOME/.profile
-ln -sv $DOTF/gitconfig $HOME/.gitconfig
-ln -sv $DOTF/bashrc $HOME/.bashrc
-ln -sv $DOTF/bin $HOME/bin
-ln -sv $DOTF/config $HOME/.config
-ln -sv $DOTF/fonts $HOME/.fonts
-ln -sv $DOTF/venvs $HOME/venvs
-ln -sv $DOTF/tmux.conf $HOME/.tmux.conf
-ln -sv $DOTF/Xresources $HOME/.Xresources
-ln -sv $DOTF/urxvt $HOME/.urxvt
-ln -sv $DOTF/spacemacs $HOME/.spacemacs.d
+link_everything
 
 print_gr "~~~~~~~~~~~~~~ Home folder set up correctly ~~~~~~~~~~~~~\n"
 
