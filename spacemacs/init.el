9730f7a17c7971a0befefd660b80a71feee7a206
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-03-04 17:05:15 +0000

    [Spacemacs] Add megapack, switch to `material`
    and other fixes.

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -1,3 +1,5 @@
+;; Personal (edran) configuration for Spacemacs
+
 (defun dotspacemacs/layers ()
   (setq-default
    dotspacemacs-distribution 'spacemacs
@@ -5,79 +7,76 @@
    (list (expand-file-name "layers/" dotspacemacs-directory))
    dotspacemacs-configuration-layers
    '(
-
      ;; ---------- editing
-     (auto-completion :variables
-                     auto-completion-return-key-behavior 'complete
-                     auto-completion-tab-key-behavior 'cycle
-                     auto-completion-enable-help-tooltip t
-                     auto-completion-enable-sort-by-usage t)
+     (auto-completion
+      :variables
+      auto-completion-return-key-behavior 'complete
+      auto-completion-tab-key-behavior 'cycle
+      auto-completion-enable-help-tooltip t
+      auto-completion-enable-sort-by-usage t)
 
-     (ibuffer :variables ibuffer-group-buffers-by 'projects)
+     (ibuffer
+      :variables
+      ibuffer-group-buffers-by 'projects)
 
-     colors
-     better-defaults
-     syntax-checking
-     (spell-checking :variables
-                     spell-checking-enable-auto-dictionary t)
+     (colors)
+     (better-defaults)
+     (syntax-checking)
+     (spell-checking
+      :variables
+      spell-checking-enable-auto-dictionary t)
 
      ;; ---------- tools
+     (themes-megapack)
      (org)
      (version-control)
-     (git :variables
-          git-magit-status-fullscreen t
-          magit-save-repository-buffers 'dontask
-          magit-revert-buffers 'silent
-          ;; Commit counts for all branches/tags
-          magit-refs-show-commit-count 'all)
-     (github :variables
-             gh-profile-default-profile "edran")
-     (shell :variables
-            shell-default-shell 'shell
-            shell-default-height 30
-            shell-default-position 'bottom)
+     (git
+      :variables
+      git-magit-status-fullscreen t
+      magit-save-repository-buffers 'dontask
+      magit-revert-buffers 'silent
+      ;; Commit counts for all branches/tags
+      magit-refs-show-commit-count 'all)
+     (github
+      :variables
+      gh-profile-default-profile "edran")
+     (shell
+      :variables
+      shell-default-shell 'shell
+      shell-default-height 30
+      shell-default-position 'bottom)
      (restclient)
      ;; dash ;; requires zeal installed on OS
 
      ;; ---------- languages
      (emacs-lisp)
      (ipython-notebook)
-     (html :variables css-indent-offset 2)
-     (latex :variables latex-enable-auto-fill t)
+     (html
+      :variables
+      css-indent-offset 2)
+     (latex
+      :variables
+      latex-enable-auto-fill t)
      (markdown)
      (python)
      (yaml)
      (lua)
-     ;; ---------- personal
 
+     ;; ---------- personal
      (edran)
      ;; (edran-torch)
      )
-   ;; List of additional packages that will be installed without being
-   ;; wrapped in a layer. If you need some configuration for these
-   ;; packages then consider to create a layer, you can also put the
-   ;; configuration in `dotspacemacs/config'.
    dotspacemacs-additional-packages '()
-   ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
-   ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
-   ;; are declared in a layer which is not a member of
-   ;; the list `dotspacemacs-configuration-layers'. (default t)
    dotspacemacs-delete-orphan-packages t))
 
 (defun dotspacemacs/init ()
   (setq-default
    dotspacemacs-editing-style 'vim
    dotspacemacs-verbose-loading nil
-   dotspacemacs-startup-banner 'official
-   dotspacemacs-startup-lists '(recents projects)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light
-                         solarized-light
-                         solarized-dark
-                         leuven
-                         monokai
-                         zenburn)
+   dotspacemacs-startup-banner nil
+   dotspacemacs-startup-lists '(recents bookmarks projects)
+   dotspacemacs-themes '(material)
    dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-default-font '("Source Code Pro"
                                :size 13
@@ -93,7 +92,7 @@
    dotspacemacs-ex-substitute-global nil
    dotspacemacs-auto-save-file-location 'cache
    dotspacemacs-use-ido nil
-   dotspacemacs-helm-resize nil
+   dotspacemacs-helm-resize t
    dotspacemacs-helm-no-header nil
    dotspacemacs-helm-position 'bottom
    dotspacemacs-enable-paste-micro-state nil
@@ -105,27 +104,30 @@
    dotspacemacs-maximized-at-startup nil
    dotspacemacs-active-transparency 90
    dotspacemacs-inactive-transparency 90
-   dotspacemacs-mode-line-unicode-symbols t
+   dotspacemacs-mode-line-unicode-symbols nil ;; non-default
    dotspacemacs-smooth-scrolling t
    dotspacemacs-smartparens-strict-mode nil
    dotspacemacs-highlight-delimiters 'all
-   dotspacemacs-persistent-server nil
-   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   dotspacemacs-persistent-server t ;; non-default
+   dotspacemacs-search-tools '("ag" "pt" "ack" "grep") ;; TODO remove pt and ack
    dotspacemacs-default-package-repository nil
    ))
 
 (defun dotspacemacs/user-init ()
-  "Initialization function for user code.
-It is called immediately after `dotspacemacs/init'.  You are free to put any
-user code."
-  (setq magit-repository-directories '("~/Git/"))
+  (setq-default
+   ;; Magit
+   magit-repository-directories '("~/Git/")
+
+   ;; Evil
+   evil-shift-round nil ;; Don't shift to nearest multiplier
+
+   ;; Whitespace mode
+   whitespace-style '(face tabs tab-mark)
+   )
   )
 
 (defun dotspacemacs/user-config ()
-  "Configuration function for user code.
- This function is called at the very end of Spacemacs initialization after
-layers configuration. You are free to put any user code."
-  (setq powerline-default-separator 'slant))
+  (setq powerline-default-separator 'alternate))
 
 ;; Use external custom file
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
