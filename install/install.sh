ae4cf2354a14b8f5adaa9f0e89444754b61d9c7e
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-04-06 17:46:57 +0100

    [CI] Add check-less installer

--- install/install.sh
+++ install/install.sh
@@ -39,7 +39,27 @@ link_everything () {
 
 # -------------------------------
 
-initial_check
+OPTIND=1
+no_check=false
+
+while getopts "h?n:" opt; do
+    case "$opt" in
+        h|\?)
+            echo "Usage:\n    `sudo ./install.sh`"
+            exit 0
+            ;;
+        n)  no_check=true
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+if [ "$no_check" == false ] ; then
+    initial_check
+fi
 
 print_bl "---- Backupping home config files\n"
 
