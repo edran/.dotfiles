69ea2b4794b9750edc1d5980e364395ea4541e50
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-07-05 16:13:48 +0100

    Add i3 (somewhat broken) workspace swap script

---
+++ dots/i3/switch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import i3
+outputs = i3.get_outputs()
+
+# set current workspace to output 0
+i3.workspace(outputs[0]['current_workspace'])
+
+# ..and move it to the other output.
+# outputs wrap, so the right of the right is left ;)
+i3.command('move', 'workspace to output left')
+
+# rinse and repeat
+i3.workspace(outputs[1]['current_workspace'])
+i3.command('move', 'workspace to output left')
