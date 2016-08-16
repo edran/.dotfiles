c3481a99c63e87fd9887fabb340c2f76bd20baf8
Nantas Nardelli <nantas.nardelli@gmail.com>
2016-08-16 19:14:17 +0100

    Fix workspace switching script

--- dots/i3/switch.py
+++ dots/i3/switch.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python
 
+# Author: Nantas Nardelli
+# License: MIT
+
 import i3
+
+
+def is_w_focused(o):
+    wss = i3.get_workspaces()
+    name = o[u'current_workspace']
+    current = None
+    for w in wss:
+        if w[u'name'] == o['current_workspace']:
+            current = w
+            break
+    return current[u'focused']
+
+
 outputs = i3.get_outputs()
 
-# set current workspace to output 0
-i3.workspace(outputs[0]['current_workspace'])
+l = []
+first = None
 
-# ..and move it to the other output.
-# outputs wrap, so the right of the right is left ;)
-i3.command('move', 'workspace to output left')
+for w in outputs:
+    if w[u'current_workspace'] is not None:
+        l.append(w)
+        if first is None:
+            first = w if is_w_focused(w) else None
 
-# rinse and repeat
-i3.workspace(outputs[1]['current_workspace'])
-i3.command('move', 'workspace to output left')
+for o in l:
+    if not is_w_focused(o):
+        i3.workspace(o['current_workspace'])
+    i3.command('move', 'workspace to output left')
+
+# Makes sure to go back to the initial window
+if not is_w_focused(first):
+    i3.workspace(first[u'current_workspace'])
