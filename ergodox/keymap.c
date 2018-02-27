cef9726b9beba7d61ebfc9ccb1f4a8ffdbec0b94
Nantas Nardelli <nantas.nardelli@gmail.com>
2018-02-27 18:28:25 +0000

    [ergodox] go back to standard main layout (plus enchancements)

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -23,19 +23,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  | TT(2)|           | TT(2)|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   `    |   Q  |   W  |   E  |   R  |   T  | TT(1)|           | TT(1)|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  | TT(1)|           | TT(1)|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------| Alt  |           | Alt  |------+------+------+------+------+--------|
  * | Shift( |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| Shift) |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      | LGUI |                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |   `  |      |      |      | LGUI |                                       | LGui |   [  |   ]  |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      | DEL  |
+ *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 | Space| Tab  |------|       |------|BackSP|Enter |
+ *                                 |BackSp| Del  |------|       |------|Enter | Space|
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -44,22 +44,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQUAL,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TT(NAVIGATION),
-        KC_GRAVE,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TT(SYMBOLS),
+        KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TT(SYMBOLS),
         CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,
-        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_LGUI,
+        KC_GRAVE,         KC_TRNS, KC_TRNS, KC_TRNS, KC_LGUI,
                                               KC_TRNS, KC_TRNS,
                                                        KC_TRNS,
-                                      KC_SPC, KC_TAB,  KC_TRNS,
+                                   KC_BSPACE, KC_DEL,  KC_TRNS,
         // right hand
         TT(NAVIGATION),    KC_6,    KC_7,    KC_8,        KC_9,        KC_0,           KC_MINUS,
         TT(SYMBOLS),       KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,           KC_BSLASH,
                            KC_H,    KC_J,    KC_K,        KC_L,        KC_SCOLON,      RCTL_T(KC_QUOTE),
         KC_LALT,           KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLASH,       KC_RSPC,
                                     KC_LGUI, KC_LBRACKET, KC_RBRACKET, KC_TRNS,        KC_TRNS,
-        KC_TRNS,     KC_DELETE,
+        KC_TRNS, KC_TRNS,
         KC_TRNS,
-        KC_TRNS,     KC_BSPACE,     KC_ENTER
+        KC_TRNS, KC_ENTER, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
