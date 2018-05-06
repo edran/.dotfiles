40101f7ee57a63ddc496974d28cc1ce166d1c3c6
Nantas Nardelli <nantas.nardelli@gmail.com>
2018-05-06 22:26:53 +0100

    Add cleaner tiling config, better alt setup on ergodox

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TT(SYMBOLS),
         CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,
-        KC_GRAVE,         KC_TRNS, KC_TRNS, KC_TRNS, KC_LGUI,
+        KC_GRAVE,         KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI,
                                               KC_TRNS, KC_TRNS,
                                                        KC_TRNS,
                                    KC_BSPACE, KC_DEL,  KC_TRNS,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TT(SYMBOLS),       KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,           KC_BSLASH,
                            KC_H,    KC_J,    KC_K,        KC_L,        KC_SCOLON,      RCTL_T(KC_QUOTE),
         KC_LALT,           KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLASH,       KC_RSPC,
-                                    KC_LGUI, KC_LBRACKET, KC_RBRACKET, KC_TRNS,        KC_TRNS,
+                                    KC_LGUI, KC_LALT,     KC_LBRACKET, KC_RBRACKET,    KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
         KC_TRNS, KC_ENTER, KC_SPC
@@ -108,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |Accel0|      |           |      |      | Home | End  |      |      |        |
+ * |        |      |      | MsUp |      |Accel0|      |           |      |      | End  | Home |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|Accel1|------|           |------| Left |  Up  | Down | Right|      |        |
+ * |        |      |MsLeft|MsDown|MsRght|Accel1|------|           |------| Left | Down |  Up  | Right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |Accel2|      |           |      |      | PgUp |PgDown|      |      |        |
+ * |        |      |      |      |      |Accel2|      |           |      |      |PgDown| PgUp |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      | Lclk | Rclk |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -136,9 +136,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                             KC_MS_BTN1, KC_MS_BTN2, KC_MS_BTN3,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_HOME, KC_END,   KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_LEFT, KC_UP,   KC_DOWN,  KC_RIGHT, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_PGUP, KC_PGDOWN, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_END, KC_HOME,   KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_LEFT, KC_DOWN,   KC_UP,  KC_RIGHT, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_PGDOWN, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS,
                           KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
