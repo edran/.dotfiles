f36737c27cfc67ce8c3619b2d2b73e1eecc4aa99
Nantas Nardelli <nantas@arbitrarygravitas.com>
2023-12-27 19:14:25 +0100

    Fixup tide / fzf / eza / git and other shell stuff

--- dots/fish/conf.d/fzf.fish
+++ dots/fish/conf.d/fzf.fish
@@ -1,5 +1,4 @@
-# fzf.fish is only meant to be used in interactive mode. If not in interactive mode and not in CI, skip the config to speed up shell startup
-if not status is-interactive && test "$CI" != true
+if not status is-interactive
     exit
 end
 
