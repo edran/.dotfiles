88b4444b9674378d3aba233d65206d5002ef7ae3
Nantas Nardelli <nantas@arbitrarygravitas.com>
2024-01-28 11:16:07 +0000

    Implement some aerospace items for sketchybar

--- dots/macos/sketchybar/items/aerospace.sh
+++ dots/macos/sketchybar/items/aerospace.sh
@@ -1,14 +1,26 @@
+#!/usr/bin/env bash
+
+SPACE_ICONS=("󰇧" "" "" "" "󰀻" "󰀻" "󰀻" "" "")
+
 sketchybar --add event aerospace_workspace_change
 
+# TODO: aerospace for now doesn't seem to support list windows in a particular
+# workspace. If we had that, we could modify the color of the icon depending on
+# whether the workspace has anythign in it.
+# TODO: likewise for the current mode.
 for sid in $(aerospace list-workspaces --all); do
+    icon_index=$((sid - 1))
+    icon="${SPACE_ICONS[$icon_index]}"
+
     sketchybar --add item space.$sid left \
         --subscribe space.$sid aerospace_workspace_change \
         --set space.$sid \
-        background.color=0x44ffffff \
-        background.corner_radius=5 \
-        background.height=20 \
-        background.drawing=off \
-        label="$sid" \
+        icon="$icon" \
+        icon.font="$FONT:Bold:18.0" \
+        icon.color=$YELLOW \
+        icon.padding_left=3 \
+        icon.padding_right=1 \
+        label.drawing=off \
         click_script="aerospace workspace $sid" \
         script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
--- dots/macos/sketchybar/plugins/aerospace.sh
+++ dots/macos/sketchybar/plugins/aerospace.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+source "$CONFIG_DIR/colors.sh"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME \
+        icon.color=$RED
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME \
+        icon.color=$YELLOW
 fi
