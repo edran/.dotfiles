9a1fcab3cbecb4a3b072d1355bf945baa6031497
Nantas Nardelli <nantas@arbitrarygravitas.com>
2024-04-26 17:51:09 +0000

    Only load macos fish config on macos

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -1,6 +1,21 @@
-if status --is-login
-  source (status dirname)/login.fish
-end
+set -x LANG "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
+
+set -gx CACHEDIR $HOME/.cache
+set -gx CONFIGDIR $HOME/.config
+set -gx LOCALDIR $HOME/.local
+
+set -gx ORGDIR "$HOME/mcc"
+set -gx LABDIR "$HOME/lab"
+set -gx DOTSREPO "$HOME/.dotfiles"
+
+# ASPELL config
+set -gx ASPELL_CONF "home-dir $HOME/.config/aspell/"
+
+fish_add_path -g "$HOME/bin"
+fish_add_path -g "$HOME/.emacs.d/bin"
+fish_add_path -g "$HOME/.config/emacs/bin"
+fish_add_path -g "$HOME/.local/bin"
 
 if test -f "$HOME/.private.fish"
     source "$HOME/.private.fish"
@@ -8,10 +23,16 @@ end
 
 if status --is-interactive
   for file in (status dirname)/interactive/*.fish
+    if test (uname) != "Darwin"
+      # We don't want to source macos.fish if we are not on a mac.
+      if string match -q "*/macos.fish" $file
+        continue
+      end
+    end
+
     source $file
   end
-
-  zoxide init fish | source
+    zoxide init fish | source
 
   abbr --add t 'zellij'
   abbr --add ta 'zellij attach -c main'
