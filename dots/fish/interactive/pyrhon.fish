ed681a7636e3e5bffd9788d1daceda9ed9297846
Nantas Nardelli <nantas@arbitrarygravitas.com>
2024-07-25 17:42:07 +0000

    Remove pyenv

--- dots/fish/interactive/pyrhon.fish
+++ dots/fish/interactive/pyrhon.fish
@@ -1,6 +1,4 @@
-set -gx PYENV_ROOT "$HOME/.pyenv"
-fish_add_path -g $PYENV_ROOT/.pyenv
-
-pyenv init - | source
+# fish_add_path -g "/fsx/users/nantas/tools/rye/shims"
 
 set -gx PYTHONBREAKPOINT ipdb.set_trace
+
